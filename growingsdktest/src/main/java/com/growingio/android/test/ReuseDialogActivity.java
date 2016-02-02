f3616e3943b7b60666f17f89890cff9f886f3da6
lishaojie <super@joker.li>
2016-02-02 18:09:03 +0800

    Correct page event titles

---
+++ growingsdktest/src/main/java/com/growingio/android/test/ReuseDialogActivity.java
@@ -0,0 +1,16 @@
+package com.growingio.android.test;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by lishaojie on 16/2/1.
+ */
+public class ReuseDialogActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Handler handler = new Handler();
+
+    }
+}
